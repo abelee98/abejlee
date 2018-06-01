55e3487f18ec6f47ed79871ee328deea99cb0c35
Abraham Lee <abrahamlee@Abrahams-MBP-2.T-mobile.com>
2018-05-31 20:24:34 -0400

    changes to navbar

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
-$(document).ready(function() {
-  
+$(document).ready(scroll_nav)
+$(window).scroll(scroll_nav)
+
+function scroll_nav() {
     var scrollLink = $('.scroll');
+    var scrollTop = 0
+    scrollTop = $(window).scrollTop();
+    console.log(scrollTop)
     
-    // Smooth scrolling
-    scrollLink.click(function(e) {
-      e.preventDefault();
-      $('body,html').animate({
-        scrollTop: $(this.hash).offset().top
-      }, 1000);
-    });
+    if (scrollTop > 100) {
+      console.log("greater")
+      $('#navbar').addClass('scroll_nav')
+      $('.scroll').css({
+        "color": "#474547"
+      })
+      $('.navbar').css({
+        "padding-top": "10px"
+      })
+    } else if (scrollTop < 100) {
+      $('#navbar').removeClass('scroll_nav')
+      $('.scroll').css({
+        "color": "white",
+        "padding-top": "0px",
+        "padding-bottom": "0px",
+      })
+      $('.navbar').css({
+        "padding-top": "40px"
+      })
+    }
+    // // Smooth scrolling
+    // scrollLink.click(function(e) {
+    //   e.preventDefault();
+    //   $('html, body').animate({
+    //     scrollTop: $(this.hash).offset().top
+    //   }, 1000);
+    // });
     
-  })
+}
